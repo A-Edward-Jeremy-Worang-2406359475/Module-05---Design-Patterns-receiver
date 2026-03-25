c90d34ef5804f0cfd13712dfe0942226bea503ff
keseter <worang19@gmail.com>
2026-03-26 00:41:28 +0700

    Implement list_messages function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -94,4 +94,8 @@ return match request {
     return Ok(subscriber_result);
     }
 
+    pub fn list_messages() -> Result<Vec<String>> {
+    return Ok(NotificationRepository::list_all_as_string());
+    }
+
 }
